d72e386993d77dbac2de60b7916ac14df68e72a6
TelluriumCrystal <admin@andrewbennet1.com>
2019-02-06 05:06:01 -0500

    Added enums for errors and USART selection

--- xa4u_usart/xa4u_usart.c
+++ xa4u_usart/xa4u_usart.c
@@ -8,215 +8,230 @@
 #include "xa4u_usart.h"
 
 // Define USART data buffers
-static volatile FIFOBuffer C0_out_buffer;
-static volatile FIFOBuffer C1_out_buffer;
-static volatile FIFOBuffer D0_out_buffer;
-static volatile FIFOBuffer D1_out_buffer;
-static volatile FIFOBuffer E0_out_buffer;
-static volatile FIFOBuffer C0_in_buffer;
-static volatile FIFOBuffer C1_in_buffer;
-static volatile FIFOBuffer D0_in_buffer;
-static volatile FIFOBuffer D1_in_buffer;
-static volatile FIFOBuffer E0_in_buffer;
+static volatile FIFOBuffer_t C0_out_buffer;
+static volatile FIFOBuffer_t C1_out_buffer;
+static volatile FIFOBuffer_t D0_out_buffer;
+static volatile FIFOBuffer_t D1_out_buffer;
+static volatile FIFOBuffer_t E0_out_buffer;
+static volatile FIFOBuffer_t C0_in_buffer;
+static volatile FIFOBuffer_t C1_in_buffer;
+static volatile FIFOBuffer_t D0_in_buffer;
+static volatile FIFOBuffer_t D1_in_buffer;
+static volatile FIFOBuffer_t E0_in_buffer;
 
-// Internal function to write to FIFO buffer. Returns 1 if buffer is full.
-static uint8_t FIFOWrite(volatile FIFOBuffer *buffer, uint8_t byte)
+// Internal function to write to FIFO buffer. Returns XA4U_USART_ERR_FULL if buffer is full.
+static XA4U_USART_ERR_t FIFOWrite(volatile FIFOBuffer_t *buffer, uint8_t byte)
 {
 	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) // Ensure buffer is accessed atomically
 	{
-		if((*buffer).buffer_head + 1 == (*buffer).buffer_tail || ((*buffer).buffer_head == USART_BUFFER_SIZE - 1 && (*buffer).buffer_tail == 0)) return 1; // No room in buffer
+		if((*buffer).buffer_head + 1 == (*buffer).buffer_tail || ((*buffer).buffer_head == USART_BUFFER_SIZE - 1 && (*buffer).buffer_tail == 0)) return XA4U_USART_ERR_FULL; // No room in buffer
 		(*buffer).buffer[(*buffer).buffer_head] = byte;
 		if((*buffer).buffer_head == USART_BUFFER_SIZE - 1) (*buffer).buffer_head = 0; // Handle buffer wrap-around
 		else (*buffer).buffer_head++;
 		(*buffer).buffer_size++;
 	}
-	return 0;
+	return XA4U_USART_ERR_NONE;
 }
 
-// Internal function to read from FIFO buffer. Returns 1 if buffer is empty.
-static uint8_t FIFORead(volatile FIFOBuffer *buffer, uint8_t *byte)
+// Internal function to read from FIFO buffer. Returns XA4U_USART_ERR_EMTY if buffer is empty.
+static XA4U_USART_ERR_t FIFORead(volatile FIFOBuffer_t *buffer, uint8_t *byte)
 {
 	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) // Ensure buffer is accessed atomically
 	{
-		if((*buffer).buffer_head == (*buffer).buffer_tail) return 1; // Buffer empty
+		if((*buffer).buffer_head == (*buffer).buffer_tail) return XA4U_USART_ERR_EMTY; // Buffer empty
 		*byte = (*buffer).buffer[(*buffer).buffer_tail];
 		if((*buffer).buffer_tail == USART_BUFFER_SIZE - 1) (*buffer).buffer_tail = 0; // Handle buffer wrap-around
 		else (*buffer).buffer_tail++;
 		(*buffer).buffer_size--;
 	}
-	return 0;
+	return XA4U_USART_ERR_NONE;
 }
 
-uint8_t USART_OutputBufferAdd(uint8_t usart, uint8_t data[], uint8_t data_size)
+XA4U_USART_ERR_t USART_OutputBufferAdd(XA4U_USART_t usart, uint8_t data[], uint8_t data_size)
 {
-	uint8_t error = 0;
+	XA4U_USART_ERR_t error = XA4U_USART_ERR_NONE;
 	switch(usart)
 	{
-		case 0:
-		if(USART_BUFFER_SIZE - C0_out_buffer.buffer_size < data_size) return 2; // Return 2 if not enough room in buffer for 'data[]'
-		for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
-		{
-			error = FIFOWrite(&C0_out_buffer, data[i]);
-			if(error == 1) break; // Stop filling buffer if full
-		}
-		if(USARTC0.STATUS & USART_DREIF_bm)  // Start shifting data into hardware buffers if hardware buffers are empty
-		{
-			USART_SendByte(0);
-		}
-		if(error == 1) return 3; // Return 3 if buffer fills unexpectedly
-		return 0;
-		case 1:
-		if(USART_BUFFER_SIZE - C1_out_buffer.buffer_size < data_size) return 2; // Return 2 if not enough room in buffer for 'data[]'
-		for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
-		{
-			error = FIFOWrite(&C1_out_buffer, data[i]);
-			if(error == 1) break; // Stop filling buffer if full
-		}
-		if(USARTC1.STATUS & USART_DREIF_bm)  // Start shifting data into hardware buffers if hardware buffers are empty
-		{
-			USART_SendByte(1);
-		}
-		if(error == 1) return 3; // Return 3 if buffer fills unexpectedly
-		return 0;
-		case 2:
-		if(USART_BUFFER_SIZE - D0_out_buffer.buffer_size < data_size) return 2; // Return 2 if not enough room in buffer for 'data[]'
-		for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
-		{
-			error = FIFOWrite(&D0_out_buffer, data[i]);
-			if(error == 1) break; // Stop filling buffer if full
-		}
-		if(USARTD0.STATUS & USART_DREIF_bm)  // Start shifting data into hardware buffers if hardware buffers are empty
-		{
-			USART_SendByte(2);
-		}
-		if(error == 1) return 3; // Return 3 if buffer fills unexpectedly
-		return 0;
-		case 3:
-		if(USART_BUFFER_SIZE - D1_out_buffer.buffer_size < data_size) return 2; // Return 2 if not enough room in buffer for 'data[]'
-		for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
-		{
-			error = FIFOWrite(&D1_out_buffer, data[i]);
-			if(error == 1) break; // Stop filling buffer if full
-		}
-		if(USARTD1.STATUS & USART_DREIF_bm)  // Start shifting data into hardware buffers if hardware buffers are empty
-		{
-			USART_SendByte(3);
-		}
-		if(error == 1) return 3; // Return 3 if buffer fills unexpectedly
-		return 0;
-		case 4:
-		if(USART_BUFFER_SIZE - E0_out_buffer.buffer_size < data_size) return 2; // Return 2 if not enough room in buffer for 'data[]'
-		for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
-		{
-			error = FIFOWrite(&E0_out_buffer, data[i]);
-			if(error == 1) break; // Stop filling buffer if full
-		}
-		if(USARTE0.STATUS & USART_DREIF_bm)  // Start shifting data into hardware buffers if hardware buffers are empty
-		{
-			USART_SendByte(4);
-		}
-		if(error == 1) return 3; // Return 3 if buffer fills unexpectedly
-		return 0;
+		case XA4U_USART_C0:
+			if(USART_BUFFER_SIZE - C0_out_buffer.buffer_size < data_size) return XA4U_USART_ERR_FULL; // Not enough room in buffer for 'data[]'
+			for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
+			{
+				error = FIFOWrite(&C0_out_buffer, data[i]);
+				if(error == XA4U_USART_ERR_FULL) return XA4U_USART_ERR_UEFULL; // Stop filling buffer if full, return unexpected full error
+			}
+			if(USARTC0.STATUS & USART_DREIF_bm)  // Start shifting data into hardware buffers if hardware buffers are empty; initiates interrupt-driven TX until TX buffer is empty again
+			{
+				USART_SendByte(XA4U_USART_C0);
+			}
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_C1:
+			if(USART_BUFFER_SIZE - C1_out_buffer.buffer_size < data_size) return XA4U_USART_ERR_FULL; // Not enough room in buffer for 'data[]'
+			for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
+			{
+				error = FIFOWrite(&C1_out_buffer, data[i]);
+				if(error == XA4U_USART_ERR_FULL) return XA4U_USART_ERR_UEFULL; // Stop filling buffer if full, return unexpected full error
+			}
+			if(USARTC1.STATUS & USART_DREIF_bm)  // Start shifting data into hardware buffers if hardware buffers are empty; initiates interrupt-driven TX until TX buffer is empty again
+			{
+				USART_SendByte(XA4U_USART_C1);
+			}
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_D0:
+			if(USART_BUFFER_SIZE - D0_out_buffer.buffer_size < data_size) return XA4U_USART_ERR_FULL; // Not enough room in buffer for 'data[]'
+			for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
+			{
+				error = FIFOWrite(&D0_out_buffer, data[i]);
+				if(error == XA4U_USART_ERR_FULL) return XA4U_USART_ERR_UEFULL; // Stop filling buffer if full, return unexpected full error
+			}
+			if(USARTD0.STATUS & USART_DREIF_bm)  // Start shifting data into hardware buffers if hardware buffers are empty; initiates interrupt-driven TX until TX buffer is empty again
+			{
+				USART_SendByte(XA4U_USART_D0);
+			}
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_D1:
+			if(USART_BUFFER_SIZE - D1_out_buffer.buffer_size < data_size) return XA4U_USART_ERR_FULL; // Not enough room in buffer for 'data[]'
+			for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
+			{
+				error = FIFOWrite(&D1_out_buffer, data[i]);
+				if(error == XA4U_USART_ERR_FULL) return XA4U_USART_ERR_UEFULL; // Stop filling buffer if full, return unexpected full error
+			}
+			if(USARTD1.STATUS & USART_DREIF_bm)  // Start shifting data into hardware buffers if hardware buffers are empty; initiates interrupt-driven TX until TX buffer is empty again
+			{
+				USART_SendByte(XA4U_USART_D1);
+			}
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_E0:
+			if(USART_BUFFER_SIZE - E0_out_buffer.buffer_size < data_size) return XA4U_USART_ERR_FULL; // Not enough room in buffer for 'data[]'
+			for(uint8_t i = 0; i < data_size; i++)  // Add data to buffer
+			{
+				error = FIFOWrite(&E0_out_buffer, data[i]);
+				if(error == XA4U_USART_ERR_FULL) return XA4U_USART_ERR_UEFULL; // Stop filling buffer if full, return unexpected full error
+			}
+			if(USARTE0.STATUS & USART_DREIF_bm)  // Start shifting data into hardware buffers if hardware buffers are empty; initiates interrupt-driven TX until TX buffer is empty again
+			{
+				USART_SendByte(XA4U_USART_E0);
+			}
+			return XA4U_USART_ERR_NONE;
 	}
-	return 1;  // Return 1 if 'usart' is invalid
+	return XA4U_USART_ERR_INVD; // Return invalid XA4U_USART_t error if execution somehow reaches this point
 }
 
-uint8_t USART_InputBufferAdd(uint8_t usart, uint8_t data)
+XA4U_USART_ERR_t USART_InputBufferAdd(XA4U_USART_t usart, uint8_t data)
 {
-	uint8_t error = 0;
+	XA4U_USART_ERR_t error = XA4U_USART_ERR_NONE;
 	switch(usart)
 	{
-		case 0:
-		error = FIFOWrite(&C0_in_buffer, data);
-		if(error == 1) return 2; // Return 2 if buffer full
-		return 0;
-		case 1:
-		error = FIFOWrite(&C1_in_buffer, data);
-		if(error == 1) return 2; // Return 2 if buffer full
-		return 0;
-		case 2:
-		error = FIFOWrite(&D0_in_buffer, data);
-		if(error == 1) return 2; // Return 2 if buffer full
-		return 0;
-		case 3:
-		error = FIFOWrite(&D1_in_buffer, data);
-		if(error == 1) return 2; // Return 2 if buffer full
-		return 0;
-		case 4:
-		error = FIFOWrite(&E0_in_buffer, data);
-		if(error == 1) return 2; // Return 2 if buffer full
-		return 0;
+		case XA4U_USART_C0:
+			error = FIFOWrite(&C0_in_buffer, data);
+			if(error == XA4U_USART_ERR_FULL) return XA4U_USART_ERR_FULL; // Buffer full
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_C1:
+			error = FIFOWrite(&C1_in_buffer, data);
+			if(error == XA4U_USART_ERR_FULL) return XA4U_USART_ERR_FULL; // Buffer full
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_D0:
+			error = FIFOWrite(&D0_in_buffer, data);
+			if(error == XA4U_USART_ERR_FULL) return XA4U_USART_ERR_FULL; // Buffer full
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_D1:
+			error = FIFOWrite(&D1_in_buffer, data);
+			if(error == XA4U_USART_ERR_FULL) return XA4U_USART_ERR_FULL; // Buffer full
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_E0:
+			error = FIFOWrite(&E0_in_buffer, data);
+			if(error == XA4U_USART_ERR_FULL) return XA4U_USART_ERR_FULL; // Buffer full
+			return XA4U_USART_ERR_NONE;
 	}
-	return 1;  // Return 1 if 'usart' is invalid
+	return XA4U_USART_ERR_INVD; // Return invalid XA4U_USART_t error if execution somehow reaches this point
 }
 
-uint8_t USART_ReadByte(uint8_t usart, uint8_t *data)
+XA4U_USART_ERR_t USART_ReadByte(XA4U_USART_t usart, uint8_t *data)
 {
-	uint8_t error = 0;
-	uint8_t byte = NULL;
+	XA4U_USART_ERR_t error = XA4U_USART_ERR_NONE;
+	uint8_t byte = 0x00;
 	switch(usart)
 	{
-		case 0:
-		error = FIFORead(&C0_in_buffer, &byte);
-		if(error == 1) return 2; // Return 2 if buffer empty
-		*data = byte;
-		return 0;
-		case 1:
-		error = FIFORead(&C1_in_buffer, &byte);
-		if(error == 1) return 2; // Return 2 if buffer empty
-		return 0;
-		case 2:
-		error = FIFORead(&D0_in_buffer, &byte);
-		if(error == 1) return 2; // Return 2 if buffer empty
-		return 0;
-		case 3:
-		error = FIFORead(&D1_in_buffer, &byte);
-		if(error == 1) return 2; // Return 2 if buffer empty
-		return 0;
-		case 4:
-		error = FIFORead(&E0_in_buffer, &byte);
-		if(error == 1) return 2; // Return 2 if buffer empty
-		return 0;
+		case XA4U_USART_C0:
+			error = FIFORead(&C0_in_buffer, &byte);
+			if(error == XA4U_USART_ERR_EMTY) return XA4U_USART_ERR_EMTY; // Buffer empty
+			*data = byte;
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_C1:
+			error = FIFORead(&C1_in_buffer, &byte);
+			if(error == XA4U_USART_ERR_EMTY) return XA4U_USART_ERR_EMTY; // Buffer empty
+			*data = byte;
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_D0:
+			error = FIFORead(&D0_in_buffer, &byte);
+			if(error == XA4U_USART_ERR_EMTY) return XA4U_USART_ERR_EMTY; // Buffer empty
+			*data = byte;
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_D1:
+			error = FIFORead(&D1_in_buffer, &byte);
+			if(error == XA4U_USART_ERR_EMTY) return XA4U_USART_ERR_EMTY; // Buffer empty
+			*data = byte;
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_E0:
+			error = FIFORead(&E0_in_buffer, &byte);
+			if(error == XA4U_USART_ERR_EMTY) return XA4U_USART_ERR_EMTY; // Buffer empty
+			*data = byte;
+			return XA4U_USART_ERR_NONE;
 	}
-	return 1;  // Return 1 if 'usart' is invalid
+	return XA4U_USART_ERR_INVD; // Return invalid XA4U_USART_t error if execution somehow reaches this point
 }
 
-uint8_t USART_SendByte(uint8_t usart)
+XA4U_USART_ERR_t USART_SendByte(XA4U_USART_t usart)
 {
-	uint8_t error = 0;
-	uint8_t data = NULL;
+	XA4U_USART_ERR_t error = XA4U_USART_ERR_NONE;
+	uint8_t data = 0x00;
 	switch(usart)
 	{
-		case 0:
-		if (!(USARTC0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
-		error = FIFORead(&C0_out_buffer, &data);
-		if (error == 1) return 3; // Return 3 if buffer is empty
-		USARTC0.DATA = data;
-		return 0;
-		case 1:
-		if (!(USARTC0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
-		error = FIFORead(&C1_out_buffer, &data);
-		if (error == 1) return 3; // Return 3 if buffer is empty
-		USARTC0.DATA = data;
-		return 0;
-		case 2:
-		if (!(USARTC0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
-		error = FIFORead(&D0_out_buffer, &data);
-		if (error == 1) return 3; // Return 3 if buffer is empty
-		USARTC0.DATA = data;
-		return 0;
-		case 3:
-		if (!(USARTC0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
-		error = FIFORead(&D1_out_buffer, &data);
-		if (error == 1) return 3; // Return 3 if buffer is empty
-		USARTC0.DATA = data;
-		return 0;
-		case 4:
-		if (!(USARTC0.STATUS & USART_DREIF_bm)) return 2;  // Return 2 if USART is not ready
-		error = FIFORead(&E0_out_buffer, &data);
-		if (error == 1) return 3; // Return 3 if buffer is empty
-		USARTC0.DATA = data;
-		return 0;
+		case XA4U_USART_C0:
+			if (!(USARTC0.STATUS & USART_DREIF_bm)) return XA4U_USART_ERR_HWNRDY;  // USART hardware is not ready
+			error = FIFORead(&C0_out_buffer, &data);
+			if (error == XA4U_USART_ERR_EMTY) return XA4U_USART_ERR_EMTY; // Buffer is empty
+			USARTC0.DATA = data;
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_C1:
+			if (!(USARTC1.STATUS & USART_DREIF_bm)) return XA4U_USART_ERR_HWNRDY;  // USART hardware is not ready
+			error = FIFORead(&C1_out_buffer, &data);
+			if (error == XA4U_USART_ERR_EMTY) return XA4U_USART_ERR_EMTY; // Buffer is empty
+			USARTC1.DATA = data;
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_D0:
+			if (!(USARTD0.STATUS & USART_DREIF_bm)) return XA4U_USART_ERR_HWNRDY;  // USART hardware is not ready
+			error = FIFORead(&D0_out_buffer, &data);
+			if (error == XA4U_USART_ERR_EMTY) return XA4U_USART_ERR_EMTY; // Buffer is empty
+			USARTD0.DATA = data;
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_D1:
+			if (!(USARTD1.STATUS & USART_DREIF_bm)) return XA4U_USART_ERR_HWNRDY;  // USART hardware is not ready
+			error = FIFORead(&D1_out_buffer, &data);
+			if (error == XA4U_USART_ERR_EMTY) return XA4U_USART_ERR_EMTY; // Buffer is empty
+			USARTD1.DATA = data;
+			return XA4U_USART_ERR_NONE;
+
+		case XA4U_USART_E0:
+			if (!(USARTE0.STATUS & USART_DREIF_bm)) return XA4U_USART_ERR_HWNRDY;  // USART hardware is not ready
+			error = FIFORead(&E0_out_buffer, &data);
+			if (error == XA4U_USART_ERR_EMTY) return XA4U_USART_ERR_EMTY; // Buffer is empty
+			USARTE0.DATA = data;
+			return XA4U_USART_ERR_NONE;
 	}
-	return 1;  // Return 1 if 'usart' is invalid
+	return XA4U_USART_ERR_INVD; // Return invalid XA4U_USART_t error if execution somehow reaches this point
 }
--- xa4u_usart/xa4u_usart.h
+++ xa4u_usart/xa4u_usart.h
@@ -16,60 +16,75 @@
 #define USART_BUFFER_SIZE 128
 #endif
 
-// FIFO Buffer struct definition
-typedef struct
+// FIFO Buffer struct definition - used to construct a single FIFO buffer
+typedef struct FIFOBuffer_struct
 {
 	uint8_t buffer[USART_BUFFER_SIZE];
 	uint8_t buffer_head;
 	uint8_t buffer_tail;
 	uint8_t buffer_size;
-} FIFOBuffer;
+} FIFOBuffer_t;
+
+// USART enum definition - used to select which USART a method is targeted at
+typedef enum XA4U_USART_enum
+{
+	XA4U_USART_C0,
+	XA4U_USART_C1,
+	XA4U_USART_D0,
+	XA4U_USART_D1,
+	XA4U_USART_E0
+} XA4U_USART_t;
+
+// Error enum definition - used for error reporting
+typedef enum XA4U_USART_ERR_enum
+{
+	XA4U_USART_ERR_NONE,	/* No error */
+	XA4U_USART_ERR_FULL,	/* Buffer full; data not passed to buffer */
+	XA4U_USART_ERR_EMTY,	/* Buffer empty; no data retrieved from buffer */
+	XA4U_USART_ERR_HWNRDY,	/* TX hardware not ready; data not removed from buffer or passed to hardware buffer */
+	XA4U_USART_ERR_UEFULL,	/* CRITICAL ERROR! Buffer filled unexpectedly after passing a check to prevent this */
+	XA4U_USART_ERR_UEEMPTY,	/* CRITICAL ERROR! Buffer unexpectedly empty after passing a check to prevent this */
+	XA4U_USART_ERR_INVD		/* CRITICAL ERROR! Invalid value passed as XA4U_USART_t; did you pass an int by mistake? */
+} XA4U_USART_ERR_t;
 
 /*  Adds an array of data to the specified USART's output buffer.
  *  INPUTS:
- *		'usart'     - USART buffer the data should be added to. Valid values are 0-4, representing USARTs C0, C1, D0, D1, and E0 in order
+ *		'usart'     - USART buffer the data should be added to
  *		'data[]'    - Data to be added to the buffer
  *		'data_size' - Length of the array passed to 'data[]'
  *  RETURN:
- *		0 - (NO ERROR) Data added to USART buffer successfully
- *		1 - (ERROR) Invalid value passed to 'usart'
- *		2 - (ERROR) Not enough room in USART buffer to fit 'data[]'
- *		3 - (ERROR) Buffer unexpectedly filled while shifting 'data[]' into buffer (only part of 'data[]' shifted)
+ *		XA4U_USART_ERR_t - Error reporting enum; see above for definitions
  */
-uint8_t USART_OutputBufferAdd(uint8_t usart, uint8_t data[], uint8_t data_size);
+XA4U_USART_ERR_t USART_OutputBufferAdd(XA4U_USART_t usart, uint8_t data[], uint8_t data_size);
+
 
 /*	Adds a byte of data to the USART input buffer. Meant to be called during USART receive ISR.
  *  INPUTS:
- *		'usart' - USART buffer the data should be added to. Valid values are 0-4, representing USARTs C0, C1, D0, D1, and E0 in order
+ *		'usart' - USART buffer the data should be added to
  *		'data'  - Data to be added to the buffer
  *  RETURN:
- *		0 - (NO ERROR) Data added to USART buffer successfully
- *      1 - (ERROR) Invalid value passed to 'usart'
- *		2 - (ERROR) USART buffer full
+ *		XA4U_USART_ERR_t - Error reporting enum; see above for definitions
  */
-uint8_t USART_InputBufferAdd(uint8_t usart, uint8_t data);
+XA4U_USART_ERR_t USART_InputBufferAdd(XA4U_USART_t usart, uint8_t data);
 
-uint8_t USART_ReadByte(uint8_t usart, uint8_t *data);
+
 /*	Reads and removes a byte of data from the USART input buffer.
  *  INPUTS:
- *		'usart' - USART buffer the data should be read from. Valid values are 0-4, representing USARTs C0, C1, D0, D1, and E0 in order
+ *		'usart' - USART buffer the data should be read from.
  *		'data'  - Data read from the buffer
  *  RETURN:
- *		0 - (NO ERROR) Data added to USART buffer successfully
- *      1 - (ERROR) Invalid value passed to 'usart'
- *		2 - (ERROR) USART buffer empty
+ *		XA4U_USART_ERR_t - Error reporting enum; see above for definitions
  */
-uint8_t USART_ReadByte(uint8_t usart, uint8_t *data);
+XA4U_USART_ERR_t USART_ReadByte(XA4U_USART_t usart, uint8_t *data);
+
 
 /*	Sends a byte of data from a USART software output buffer to a USART hardware output buffer. Meant to be called during USART send ISR.
  *  INPUTS:
  *		'usart' - USART buffer to shift data byte from software to hardware
  *  RETURN:
- *		0 - (NO ERROR) Data added to USART hardware buffer successfully
- *		1 - (ERROR) Invalid value passed to 'usart'
- *		2 - (ERROR) USART hardware buffer not ready
- *		3 - (ERROR) USART output buffer is empty
+ *		XA4U_USART_ERR_t - Error reporting enum; see above for definitions
  */
-uint8_t USART_SendByte(uint8_t usart);
+XA4U_USART_ERR_t USART_SendByte(XA4U_USART_t usart);
+
 
 #endif
