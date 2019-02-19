d2666ce253fa213b58b3e4258e05d1696a069555
TelluriumCrystal <admin@andrewbennet1.com>
2019-02-19 18:07:36 -0500

    Added description to xa4u_clock

--- xa4u_clock/xa4u_clock.h
+++ xa4u_clock/xa4u_clock.h
@@ -3,13 +3,15 @@
  * Created : 2/12/2019 5:31:33 PM
  * Author  : Andrew Bennett
  * 
- * Description : 
+ * Description : Functions for setting up the clock source. These are for use at initialization only, they are not guaranteed to work after the initial clock is set.
+                 Note that F_CPU must still be defined in main.c (e.g. "define F_CPU 8000000UL" for an 8 MHz clock).
  */
 
  #ifndef XA4U_CLOCK_H
  #define XA4U_CLOCK_H
 
  #include <avr/io.h>
+ #include <avr/delay.h>
 
  // clock enum definition - used to select which clock configuration is used
  typedef enum XA4U_CLOCK_enum
