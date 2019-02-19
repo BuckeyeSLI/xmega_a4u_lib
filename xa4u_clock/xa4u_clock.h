b0b5c5577783d497dc1aac985d9b8b9d8fb4ecb4
TelluriumCrystal <admin@andrewbennet1.com>
2019-02-19 18:10:11 -0500

    Added function prototype and fixed entire code being indented by one space

--- xa4u_clock/xa4u_clock.h
+++ xa4u_clock/xa4u_clock.h
@@ -7,17 +7,21 @@
                  Note that F_CPU must still be defined in main.c (e.g. "define F_CPU 8000000UL" for an 8 MHz clock).
  */
 
- #ifndef XA4U_CLOCK_H
- #define XA4U_CLOCK_H
+#ifndef XA4U_CLOCK_H
+#define XA4U_CLOCK_H
 
- #include <avr/io.h>
- #include <avr/delay.h>
+#include <avr/io.h>
+#include <avr/delay.h>
 
- // clock enum definition - used to select which clock configuration is used
- typedef enum XA4U_CLOCK_enum
- {
-	 XA4U_32M,				/* 32 MHz internal RC clock */
-	 XA4U_32M_DFLL			/* 32MHz internal RC clock with DFLL */
- } XA4U_CLOCK_t;
- 
- #endif
+// Clock enum definition - used to select which clock configuration is used
+typedef enum XA4U_CLOCK_enum
+{
+
+	XA4U_32M,				/* 32 MHz internal RC clock */
+	XA4U_32M_DFLL			/* 32MHz internal RC clock with DFLL */
+} XA4U_CLOCK_t;
+
+
+int clock_setup(XA4U_CLOCK_t mode);
+
+#endif
