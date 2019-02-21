3f2017df46275956730525866bdba0f5d2d091b0
TelluriumCrystal <admin@andrewbennet1.com>
2019-02-21 09:33:47 -0500

    Updated xa4u_clock description

--- xa4u_clock/xa4u_clock.h
+++ xa4u_clock/xa4u_clock.h
@@ -5,7 +5,7 @@
  * 
  * Description : Functions for setting up the clock source. These are for use at initialization only, they are not guaranteed to work if the OSC or CLK registers are
  *				 modified by other libraries.
- *               Note that F_CPU must still be defined in main.c (e.g. "define F_CPU 8000000UL" for an 8 MHz clock).
+ *               Note that F_CPU must still be defined in main.c (e.g. "define F_CPU 8000000UL" for an 8 MHz clock). It must be defined in before includes.
  */
 
 #ifndef XA4U_CLOCK_H
